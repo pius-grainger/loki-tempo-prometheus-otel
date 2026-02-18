16e9a5f8fa72d708cb9e14b2dcf14c947f459e3f
Pius <pius@Piuss-MacBook-Pro.local>
2026-02-18 10:59:53 +0000

    migrated into helm

--- modules/slo/versions.tf
+++ modules/slo/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.25"
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
     }
   }
 }
